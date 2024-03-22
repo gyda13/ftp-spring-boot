3a8e170328c34d02a062d3c8a45f07db3112a370
gyda13 <gyda2100@gmail.com>
2024-03-22 23:21:21 +0300

    Initial commit

---
+++ src/main/java/com/example/ftpspringboot/FtpSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ftpspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpSpringBootApplication.class, args);
+	}
+
+}
